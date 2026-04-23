a354e751eba492b1d9a7e5bdc87c28e785f6cc4e
mohanish-kv <mohanish.kv@acuvate.com>
2026-04-23 12:39:55 +0530

    Changed to static method

--- april23/AreaOfCircle.cs
+++ april23/AreaOfCircle.cs
@@ -2,7 +2,7 @@ using System;
 
 class Area
 {
-  public double AreaOfCircle(double r)
+  public static double AreaOfCircle(double r)
   {
     double area = Math.PI * r * r;
     return area;
@@ -11,8 +11,7 @@ class Area
   {
     Console.WriteLine("Enter the Radius of the Circle: ");
     double radius = Convert.ToDouble(Console.ReadLine());
-    Area areaCalculator = new Area();
-    double area = areaCalculator.AreaOfCircle(radius);
-    Console.WriteLine("Area of the circle with radius{0} is {1}", radius, area);
+    double area = AreaOfCircle(radius);
+    Console.WriteLine("Area of the circle with radius {0} is {1}", radius, area);
   }
 }
